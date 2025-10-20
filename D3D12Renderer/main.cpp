e944dd447ac29f462ddaad79ca8f8212c7d127db
alpakaDurumi <durams@naver.com>
2025-10-20 23:33:23 +0900

    Set Agility SDK parameters

--- D3D12Renderer/main.cpp
+++ D3D12Renderer/main.cpp
@@ -1,6 +1,9 @@
 #include <Windows.h>
 #include "Win32Application.h"
 
+extern "C" { __declspec(dllexport) extern const UINT D3D12SDKVersion = 717; }
+extern "C" { __declspec(dllexport) extern const char* D3D12SDKPath = u8".\\D3D12\\"; }
+
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd)
 {
     Renderer renderer(L"D3D12 Renderer");
