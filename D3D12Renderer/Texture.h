6a3046aa50bedd6f1b490e0521ad929364883f63
alpakaDurumi <durams@naver.com>
2025-11-20 17:02:09 +0900

    Add new constructor for texture using filePath

--- D3D12Renderer/Texture.h
+++ D3D12Renderer/Texture.h
@@ -4,6 +4,7 @@
 #include <wrl/client.h>
 
 #include <d3d12.h>
+#include "DDSTextureLoader12.h"
 
 #include <vector>
 
@@ -13,6 +14,7 @@
 #include "ResourceLayoutTracker.h"
 #include "DescriptorAllocator.h"
 #include "DescriptorAllocation.h"
+#include "Utility.h"
 
 using Microsoft::WRL::ComPtr;
 using namespace D3DHelper;
@@ -78,6 +80,136 @@ public:
         srvDesc.Texture2D.MipLevels = 1;
 
         pDevice->CreateShaderResourceView(m_texture.Get(), &srvDesc, m_allocation.GetDescriptorHandle());
+    }
+
+    Texture(
+        ID3D12Device10* pDevice,
+        CommandList& commandList,
+        DescriptorAllocator& descriptorAllocator,
+        UploadBuffer& uploadBuffer,
+        ResourceLayoutTracker& layoutTracker,
+        const std::wstring& filePath,
+        bool useBlockCompress,
+        bool flipImage,
+        bool isCubeMap)
+        : m_allocation(descriptorAllocator.Allocate())
+    {
+        // Find file and check validity
+        std::wstring ddsFilePath = Utility::RemoveFileExtension(filePath) + L".dds";
+
+        struct _stat64 ddsFileStat, srcFileStat;
+
+        bool srcFileMissing = _wstat64(filePath.c_str(), &srcFileStat) == -1;
+        bool ddsFileMissing = _wstat64(ddsFilePath.c_str(), &ddsFileStat) == -1;
+
+        if (srcFileMissing)
+        {
+            throw std::runtime_error("File not found.");
+        }
+
+        // If dds file does not exist or older than src file
+        if (ddsFileMissing || ddsFileStat.st_mtime < srcFileStat.st_mtime)
+        {
+            ConvertToDDS(filePath, useBlockCompress, flipImage);
+        }
+
+        std::unique_ptr<uint8_t[]> ddsData;
+        std::vector<D3D12_SUBRESOURCE_DATA> subresources;
+
+        ThrowIfFailed(DirectX::LoadDDSTextureFromFile(
+            pDevice,
+            ddsFilePath.c_str(),
+            &m_texture,
+            ddsData,
+            subresources));
+
+        D3D12_RESOURCE_DESC desc = m_texture->GetDesc();
+        UINT numSubresources = static_cast<UINT>(subresources.size());
+
+        layoutTracker.RegisterResource(m_texture.Get(), D3D12_BARRIER_LAYOUT_COMMON, desc.DepthOrArraySize, desc.MipLevels, desc.Format);
+
+        commandList.Barrier(
+            m_texture.Get(),
+            D3D12_BARRIER_SYNC_NONE,
+            D3D12_BARRIER_SYNC_COPY,
+            D3D12_BARRIER_ACCESS_NO_ACCESS,
+            D3D12_BARRIER_ACCESS_COPY_DEST,
+            D3D12_BARRIER_LAYOUT_COPY_DEST,
+            { 0xffffffff, 0, 0, 0, 0, 0 });
+
+        // Calculate required size for data upload
+        D3D12_PLACED_SUBRESOURCE_FOOTPRINT layouts = {};
+        UINT numRows = 0;
+        UINT64 rowSizeInBytes = 0;
+        UINT64 requiredSize = 0;
+        pDevice->GetCopyableFootprints(&desc, 0, numSubresources, 0, &layouts, &numRows, &rowSizeInBytes, &requiredSize);
+
+        auto uploadAllocation = uploadBuffer.Allocate(requiredSize, D3D12_TEXTURE_DATA_PLACEMENT_ALIGNMENT);
+
+        UpdateSubresources(pDevice, commandList, m_texture.Get(), uploadAllocation, 0, numSubresources, subresources.data());
+
+        commandList.Barrier(
+            m_texture.Get(),
+            D3D12_BARRIER_SYNC_COPY,
+            D3D12_BARRIER_SYNC_PIXEL_SHADING,
+            D3D12_BARRIER_ACCESS_COPY_DEST,
+            D3D12_BARRIER_ACCESS_SHADER_RESOURCE,
+            D3D12_BARRIER_LAYOUT_SHADER_RESOURCE,
+            { 0xffffffff, 0, 0, 0, 0, 0 });
+
+        // Describe and create a SRV for the texture.
+        D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+        srvDesc.Format = desc.Format;
+        srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+        if (desc.Dimension == D3D12_RESOURCE_DIMENSION_TEXTURE1D)
+        {
+            if (desc.DepthOrArraySize == 1)
+            {
+                srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE1D;
+                srvDesc.Texture1D.MipLevels = desc.MipLevels;
+            }
+            else
+            {
+                srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE1DARRAY;
+                srvDesc.Texture1DArray.MipLevels = desc.MipLevels;
+            }
+        }
+        else if (desc.Dimension == D3D12_RESOURCE_DIMENSION_TEXTURE2D)
+        {
+            if (desc.DepthOrArraySize == 1)
+            {
+                srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+                srvDesc.Texture2D.MipLevels = desc.MipLevels;
+            }
+            else if (desc.DepthOrArraySize % 6 == 0 && isCubeMap)
+            {
+                if (desc.DepthOrArraySize / 6 == 1)
+                {
+                    srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURECUBE;
+                    srvDesc.TextureCube.MipLevels = desc.MipLevels;
+                }
+                else
+                {
+                    srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURECUBEARRAY;
+                    srvDesc.TextureCubeArray.MipLevels = desc.MipLevels;
+                }
+            }
+            else
+            {
+                srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2DARRAY;
+                srvDesc.Texture2DArray.MipLevels = desc.MipLevels;
+            }
+        }
+        else    // TEXTURE3D
+        {
+            srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE3D;
+            srvDesc.Texture3D.MipLevels = desc.MipLevels;
+        }
+
+        pDevice->CreateShaderResourceView(m_texture.Get(), &srvDesc, m_allocation.GetDescriptorHandle());
+
+        m_width = static_cast<UINT>(desc.Width);
+        m_height = static_cast<UINT>(desc.Height);
     }
 
     D3D12_CPU_DESCRIPTOR_HANDLE GetDescriptorHandle()
