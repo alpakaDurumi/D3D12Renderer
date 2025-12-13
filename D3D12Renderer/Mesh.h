069a172a3a91fb12a7a4713c93fc054608b1e5ca
alpakaDurumi <durams@naver.com>
2025-12-13 19:19:45 +0900

    Add tangent calculation to MakeSphere function

--- D3D12Renderer/Mesh.h
+++ D3D12Renderer/Mesh.h
@@ -50,62 +50,58 @@ public:
         Mesh cube;
 
         // Create the vetex buffer
+        std::vector<Vertex> cubeVertices =
         {
-            std::vector<Vertex> cubeVertices =
-            {
-                // upper
-                {{-1.0f, 1.0f, -1.0f}, {0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
-                {{-1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
-                {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
-                {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            // upper
+            {{-1.0f, 1.0f, -1.0f}, {0.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            {{-1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            {{1.0f, 1.0f, -1.0f}, {1.0f, 1.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
 
-                // lower
-                {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
-                {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
-                {{1.0f, -1.0f, -1.0f}, {1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
-                {{1.0f, -1.0f, 1.0f}, {1.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            // lower
+            {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            {{-1.0f, -1.0f, -1.0f}, {0.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            {{1.0f, -1.0f, -1.0f}, {1.0f, 0.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
+            {{1.0f, -1.0f, 1.0f}, {1.0f, 1.0f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} },
 
-                // left
-                {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
-                {{-1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
-                {{-1.0f, 1.0f, -1.0f}, {1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
-                {{-1.0f, -1.0f, -1.0f}, {1.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
+            // left
+            {{-1.0f, -1.0f, 1.0f}, {0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
+            {{-1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
+            {{-1.0f, 1.0f, -1.0f}, {1.0f, 0.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
+            {{-1.0f, -1.0f, -1.0f}, {1.0f, 1.0f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, -1.0f} },
 
-                // right
-                {{1.0f, -1.0f, -1.0f}, {0.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
-                {{1.0f, 1.0f, -1.0f}, {0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
-                {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
-                {{1.0f, -1.0f, 1.0f}, {1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
+            // right
+            {{1.0f, -1.0f, -1.0f}, {0.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
+            {{1.0f, 1.0f, -1.0f}, {0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
+            {{1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
+            {{1.0f, -1.0f, 1.0f}, {1.0f, 1.0f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} },
 
-                // front
-                {{-1.0f, -1.0f, -1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
-                {{-1.0f, 1.0f, -1.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
-                {{1.0f, 1.0f, -1.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
-                {{1.0f, -1.0f, -1.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
+            // front
+            {{-1.0f, -1.0f, -1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
+            {{-1.0f, 1.0f, -1.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
+            {{1.0f, 1.0f, -1.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
+            {{1.0f, -1.0f, -1.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f, 0.0f} },
 
-                // back
-                {{1.0f, -1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} },
-                {{1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} },
-                {{-1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} },
-                {{-1.0f, -1.0f, 1.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} }
-            };
-            CreateVertexBuffer(pDevice, commandList, uploadBuffer, cube.m_vertexBuffer, &cube.m_vertexBufferView, cubeVertices);
-        }
+            // back
+            {{1.0f, -1.0f, 1.0f}, {0.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} },
+            {{1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} },
+            {{-1.0f, 1.0f, 1.0f}, {1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} },
+            {{-1.0f, -1.0f, 1.0f}, {1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}, {-1.0f, 0.0f, 0.0f} }
+        };
+        CreateVertexBuffer(pDevice, commandList, uploadBuffer, cube.m_vertexBuffer, &cube.m_vertexBufferView, cubeVertices);
 
         // Create the index buffer
+        std::vector<UINT32> cubeIndices =
         {
-            std::vector<UINT32> cubeIndices =
-            {
-                0, 1, 2, 0, 2, 3,
-                4, 5, 6, 4, 6, 7,
-                8, 9, 10, 8, 10, 11,
-                12, 13, 14, 12, 14, 15,
-                16, 17, 18, 16, 18, 19,
-                20, 21, 22, 20, 22, 23
-            };
-            CreateIndexBuffer(pDevice, commandList, uploadBuffer, cube.m_indexBuffer, &cube.m_indexBufferView, cubeIndices);
-            cube.m_numIndices = UINT(cubeIndices.size());
-        }
+            0, 1, 2, 0, 2, 3,
+            4, 5, 6, 4, 6, 7,
+            8, 9, 10, 8, 10, 11,
+            12, 13, 14, 12, 14, 15,
+            16, 17, 18, 16, 18, 19,
+            20, 21, 22, 20, 22, 23
+        };
+        CreateIndexBuffer(pDevice, commandList, uploadBuffer, cube.m_indexBuffer, &cube.m_indexBufferView, cubeIndices);
+        cube.m_numIndices = UINT(cubeIndices.size());
 
         return cube;
     }
@@ -122,55 +118,60 @@ public:
         const UINT numSectors = 30;
 
         // Create the vetex buffer
+        std::vector<Vertex> sphereVertices;
+        const float dPhi = XM_PI / float(numStacks);
+        const float dTheta = XM_2PI / float(numSectors);
+        for (UINT i = 0; i <= numStacks; i++)
         {
-            std::vector<Vertex> sphereVertices;
-            const float dPhi = XM_PI / float(numStacks);
-            const float dTheta = XM_2PI / float(numSectors);
-            for (UINT i = 0; i <= numStacks; i++)
+            float sinPhi, cosPhi;
+            XMScalarSinCos(&sinPhi, &cosPhi, i * dPhi);
+            // Detect north pole and set to 0.0f for eliminating floating point error
+            XMFLOAT3 stackStart(0.0f, -radius * cosPhi, i == numStacks ? 0.0f : -radius * sinPhi);  
+            for (UINT j = 0; j <= numSectors; j++)
             {
-                float sinPhi, cosPhi;
-                XMScalarSinCos(&sinPhi, &cosPhi, i * dPhi);
-                XMFLOAT3 stackStart(0.0f, 0.0f - radius * cosPhi, -radius * sinPhi);
-                for (UINT j = 0; j <= numSectors; j++)
+                float sinTheta, cosTheta;
+                XMScalarSinCos(&sinTheta, &cosTheta, j * dTheta);
+
+                Vertex v;
+                v.position.x = -stackStart.z * sinTheta;
+                v.position.y = stackStart.y;
+                v.position.z = -stackStart.z * -cosTheta;
+                v.texCoord = { float(j) / numSectors, 1.0f - float(i) / numStacks };
+                XMStoreFloat3(&v.normal, XMVector3Normalize(XMLoadFloat3(&v.position)));
+                if (stackStart.z == 0.0f)       // Detect pole
                 {
-                    float sinTheta, cosTheta;
-                    XMScalarSinCos(&sinTheta, &cosTheta, j * dTheta);
-
-                    Vertex v;
-                    v.position.x = -stackStart.z * sinTheta;
-                    v.position.y = stackStart.y;
-                    v.position.z = -stackStart.z * -cosTheta;
-                    v.texCoord = { float(j) / numSectors, 1.0f - float(i) / numStacks };
-                    XMStoreFloat3(&v.normal, XMVector3Normalize(XMLoadFloat3(&v.position)));
-
-                    sphereVertices.push_back(v);
+                    v.tangent = { 1.0f, 0.0f, 0.0f };
                 }
+                else
+                {
+                    XMStoreFloat3(&v.tangent, XMVector3Normalize(XMVectorSet(-stackStart.z * cosTheta, 0.0f, -stackStart.z * sinTheta, 0.0f)));
+                }
+
+                sphereVertices.push_back(v);
             }
-            CreateVertexBuffer(pDevice, commandList, uploadBuffer, sphere.m_vertexBuffer, &sphere.m_vertexBufferView, sphereVertices);
         }
+        CreateVertexBuffer(pDevice, commandList, uploadBuffer, sphere.m_vertexBuffer, &sphere.m_vertexBufferView, sphereVertices);
 
         // Create the index buffer
+        std::vector<UINT32> sphereIndices;
+        for (UINT i = 0; i < numStacks; i++)
         {
-            std::vector<UINT32> sphereIndices;
-            for (UINT i = 0; i < numStacks; i++)
+            for (UINT j = 0; j < numSectors; j++)
             {
-                for (UINT j = 0; j < numSectors; j++)
-                {
-                    UINT p1 = i * (numSectors + 1) + j;
-                    UINT p2 = (i + 1) * (numSectors + 1) + j;
-                    UINT p3 = p2 + 1;
-                    UINT p4 = p1 + 1;
-                    sphereIndices.push_back(p1);
-                    sphereIndices.push_back(p2);
-                    sphereIndices.push_back(p3);
-                    sphereIndices.push_back(p1);
-                    sphereIndices.push_back(p3);
-                    sphereIndices.push_back(p4);
-                }
+                UINT p1 = i * (numSectors + 1) + j;
+                UINT p2 = (i + 1) * (numSectors + 1) + j;
+                UINT p3 = p2 + 1;
+                UINT p4 = p1 + 1;
+                sphereIndices.push_back(p1);
+                sphereIndices.push_back(p2);
+                sphereIndices.push_back(p3);
+                sphereIndices.push_back(p1);
+                sphereIndices.push_back(p3);
+                sphereIndices.push_back(p4);
             }
-            CreateIndexBuffer(pDevice, commandList, uploadBuffer, sphere.m_indexBuffer, &sphere.m_indexBufferView, sphereIndices);
-            sphere.m_numIndices = UINT(sphereIndices.size());
         }
+        CreateIndexBuffer(pDevice, commandList, uploadBuffer, sphere.m_indexBuffer, &sphere.m_indexBufferView, sphereIndices);
+        sphere.m_numIndices = UINT(sphereIndices.size());
 
         return sphere;
     }
