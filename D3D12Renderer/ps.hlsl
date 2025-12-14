e03f8ac99805c54d58092e05998687adf6f1c4a2
alpakaDurumi <durams@naver.com>
2025-12-14 19:42:46 +0900

    Refactor : Shading in tangent space
    Calculate TBN matrix in VS.
    Transform vectors from world space to tangent space in VS.
    Reduce number of matrix multiplication count.

--- D3D12Renderer/ps.hlsl
+++ D3D12Renderer/ps.hlsl
@@ -4,11 +4,11 @@ SamplerState g_sampler : register(s0);
 
 struct PSInput
 {
-	float4 pos : SV_POSITION;
-	float3 posWorld : POSITION;
-	float2 texCoord : TEXCOORD;
-    float3 normalWorld : NORMAL;
-    float3 tangentWorld : TANGENT;
+    float4 pos : SV_POSITION;
+    float2 texCoord : TEXCOORD;
+    float3 posTangent : POSITION0;
+    float3 lightPosTangent : POSITION1;
+    float3 cameraPosTangent : POSITION2;
 };
 
 cbuffer MeshConstantBuffer : register(b0)
@@ -33,37 +33,26 @@ cbuffer LightConstantBuffer : register(b2)
 	float lightIntensity;
 }
 
-cbuffer CameraConstantBuffer : register(b3)
-{
-	float3 cameraPos;
-	float4x4 viewProjection;
-}
-
 float4 main(PSInput input) : SV_TARGET
 {
-	float3 toLight = normalize(lightPos - input.posWorld);
-	float3 toCamera = normalize(cameraPos - input.posWorld);
+	// Shading in tangent space
+	
+    float3 toLight = normalize(input.lightPosTangent - input.posTangent);
+    float3 toCamera = normalize(input.cameraPosTangent - input.posTangent);
 	float3 halfWay = normalize(toLight + toCamera);
 	
     float3 texColor = g_texture.Sample(g_sampler, input.texCoord * textureTileScale).rgb;
-	
-	// Normal Map
-    float3 normalSampled = g_normalMap.Sample(g_sampler, input.texCoord * textureTileScale).rgb;
-    normalSampled = 2.0f * normalSampled - 1.0f;
-    float3 N = input.normalWorld;
-    float3 T = normalize(input.tangentWorld - dot(input.tangentWorld, N) * N);
-    float3 B = normalize(cross(N, T));		// Multiply w element of input.tangent if it exists.
-    float3 normal = normalize(mul(normalSampled, float3x3(T, B, N)));
+    float3 normalTangent = g_normalMap.Sample(g_sampler, input.texCoord * textureTileScale).rgb * 2.0f - 1.0f;
 	
 	// Ambient
 	float3 ambient = materialAmbient * texColor;
 	
 	// Diffuse
-    float nDotL = max(dot(normal, toLight), 0.0f);
+    float nDotL = max(dot(normalTangent, toLight), 0.0f);
 	float3 diffuse = texColor * nDotL * lightColor * lightIntensity;
 	
 	// Specular
-    float nDotH = max(dot(normal, halfWay), 0.0f);
+    float nDotH = max(dot(normalTangent, halfWay), 0.0f);
     float3 specular = pow(nDotH, shininess) * materialSpecular * lightColor * lightIntensity;
 	
 	return float4(ambient + diffuse + specular, 1.0f);
--- D3D12Renderer/vs.hlsl
+++ D3D12Renderer/vs.hlsl
@@ -13,10 +13,10 @@ struct VSInput
 struct PSInput
 {
     float4 pos : SV_POSITION;
-    float3 posWorld : POSITION;
     float2 texCoord : TEXCOORD;
-    float3 normalWorld : NORMAL;
-    float3 tangentWorld : TANGENT;
+    float3 posTangent : POSITION0;
+    float3 lightPosTangent : POSITION1;
+    float3 cameraPosTangent : POSITION2;
 };
 
 cbuffer MeshConstantBuffer : register(b0)
@@ -25,6 +25,14 @@ cbuffer MeshConstantBuffer : register(b0)
     float4x4 inverseTranspose;
     float textureTileScale;
 };
+
+cbuffer LightConstantBuffer : register(b2)
+{
+    float3 lightPos;
+    float3 lightDir;
+    float3 lightColor;
+    float lightIntensity;
+}
 
 cbuffer CameraConstantBuffer : register(b3)
 {
@@ -36,22 +44,37 @@ PSInput main(VSInput input)
 {
     PSInput output;
 	
+    // Calculate position in world space
+    float3 posWorld;
 #ifdef INSTANCED
-	output.posWorld = mul(float4(input.pos, 1.0f), mul(world, input.instanceWorld)).xyz;
+	posWorld = mul(float4(input.pos, 1.0f), mul(world, input.instanceWorld)).xyz;
 #else
-    output.posWorld = mul(float4(input.pos, 1.0f), world).xyz;
+    posWorld = mul(float4(input.pos, 1.0f), world).xyz;
 #endif
     
-    output.pos = mul(float4(output.posWorld, 1.0f), viewProjection);
+    output.pos = mul(float4(posWorld, 1.0f), viewProjection);
     output.texCoord = input.texCoord;
     
+    // Set up TBN matrix
+    float3 T, B, N;
 #ifdef INSTANCED
-	output.normalWorld = normalize(mul(float4(input.normal, 0.0f), mul(input.instanceInverseTranspose, inverseTranspose)).xyz);
-    output.tangentWorld = normalize(mul(float4(input.tangent, 0.0f), mul(input.instanceInverseTranspose, inverseTranspose)).xyz);
+    T = normalize(mul(float4(input.tangent, 0.0f), mul(input.instanceInverseTranspose, inverseTranspose)).xyz);
+	N = normalize(mul(float4(input.normal, 0.0f), mul(input.instanceInverseTranspose, inverseTranspose)).xyz);
 #else
-    output.normalWorld = normalize(mul(float4(input.normal, 0.0f), inverseTranspose).xyz);
-    output.tangentWorld = normalize(mul(float4(input.tangent, 0.0f), inverseTranspose).xyz);
+    T = normalize(mul(float4(input.tangent, 0.0f), inverseTranspose).xyz);
+    N = normalize(mul(float4(input.normal, 0.0f), inverseTranspose).xyz);
 #endif
+    
+    T = normalize(T - dot(T, N) * N);   // Gram-Schmidt process
+    B = normalize(cross(N, T));         // Multiply w element of input.tangent if it exists. (for handedess)
+    
+    // Transform vectors from world space to tangent space.
+    // Since the TBN matrix is orthogonal, its inverse is equal to its transpose.
+    float3x3 inverseTBN = transpose(float3x3(T, B, N));
+    
+    output.posTangent = mul(posWorld, inverseTBN);
+    output.lightPosTangent = mul(lightPos, inverseTBN);
+    output.cameraPosTangent = mul(cameraPos, inverseTBN);
 	
     return output;
 }
