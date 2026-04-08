a1a00e9b5fd57ebc03fa2083ed9021b303ab9468
alpakaDurumi <durams@naver.com>
2026-04-08 18:11:09 +0900

    Add FrameUploadHeap class

---
+++ D3D12Renderer/FrameUploadHeap.h
@@ -0,0 +1,98 @@
+#pragma once
+
+#include <Windows.h>
+#include <wrl/client.h>
+
+#include <d3d12.h>
+
+#include <vector>
+#include <memory>
+#include <cstddef>
+
+#include "D3DHelper.h"
+
+using Microsoft::WRL::ComPtr;
+
+// Linear allocator using upload heap for per-frame resource
+class FrameUploadHeap
+{
+public:
+    FrameUploadHeap(ID3D12Device10* pDevice)
+        : m_pDevice(pDevice)
+    {
+        AllocatePage();
+        m_currentPageIndex = 0;
+        m_currentOffset = 0;
+    }
+
+    struct Allocation
+    {
+        void* CPUPtr;
+        D3D12_GPU_VIRTUAL_ADDRESS GPUPtr;
+    };
+
+    // Copy data from src to currentOffset, returns Allocation
+    Allocation Push(void* src, std::size_t size)
+    {
+        // If current page has not enough space
+        if (m_currentOffset + size > PAGE_SIZE)
+        {
+            // If there is no spare page
+            if (m_currentPageIndex + 1 == m_pages.size())
+                AllocatePage();
+
+            ++m_currentPageIndex;
+            m_currentOffset = 0;
+        }
+
+        auto& currentPage = m_pages[m_currentPageIndex];
+
+        Allocation alloc = {
+            static_cast<UINT8*>(currentPage->CPUBasePtr) + m_currentOffset,
+            currentPage->GPUBasePtr + m_currentOffset
+        };
+
+        memcpy(alloc.CPUPtr, src, size);
+
+        m_currentOffset += size;
+        
+        return alloc;
+    }
+
+    // Should be called at each frame start
+    void Reset()
+    {
+        m_currentPageIndex = 0;
+        m_currentOffset = 0;
+    }
+
+private:
+    static const UINT64 PAGE_SIZE = 16 * 1024 * 1024;     // 16MB
+
+    void AllocatePage()
+    {
+        auto page = std::make_unique<Page>(m_pDevice);
+        m_pages.push_back(std::move(page));
+    }
+
+    struct Page
+    {
+        Page(ID3D12Device10* pDevice)
+        {
+            D3DHelper::CreateUploadBuffer(pDevice, PAGE_SIZE, Resource);
+
+            D3D12_RANGE readRange = { 0, 0 };
+            Resource->Map(0, &readRange, &CPUBasePtr);
+            GPUBasePtr = Resource->GetGPUVirtualAddress();
+        }
+
+        ComPtr<ID3D12Resource> Resource;
+        void* CPUBasePtr;
+        D3D12_GPU_VIRTUAL_ADDRESS GPUBasePtr;
+    };
+
+    ID3D12Device10* m_pDevice;
+    std::vector<std::unique_ptr<Page>> m_pages;
+    UINT m_currentPageIndex;
+    UINT m_currentOffset;
+};
